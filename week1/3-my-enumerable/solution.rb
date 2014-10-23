099519dcd7ca21cba244967878fdb7bad801c1bd
Svetlozar Mihailov <svetliomihailov@gmail.com>
2014-10-23 19:56:07 +0300

    Only reduce method remains to be done and some corrections with the use of enum_for method to create enumerator

--- week1/3-my-enumerable/solution.rb
+++ week1/3-my-enumerable/solution.rb
@@ -1,34 +1,41 @@
 module MyEnumerable
   def map
+    return self.enum_for(:each) unless block_given?
     array = []
-    self.each { |el| array << yield(el) } if block_given?
-    array 
+    self.each { |el| array << yield(el) }
+    array
   end
 
   def filter
+    return self.enum_for(:each) unless block_given?
     array = []
-    self.each { |el| array << el if yield(el) } if block_given?
+    self.each { |el| array << el if yield(el) }
     array
   end
 
   def reject
+    return self.enum_for(:each) unless block_given?
     array = []
-    self.each { |el| array << el unless yield el } if block_given?
+    self.each { |el| array << el unless yield el }
     array
   end
 
   def reduce(initial = nil)
-    self.each { |el| initial = yield initial, el } if block_given?
+    #if initial.nil?
+      # TODO: finish this method...
+    #else
+    self.each { | el | initial = yield initial, el } if block_given?
     initial
+    #end
   end
 
   def any?
     if block_given?
-      self.each { |el| return true if yield el}
+      self.each { |el| return true if yield el }
     else
       self.each { |el| return true if el }
     end
-    return false
+    false
   end
 
   def all?
@@ -37,28 +44,25 @@ module MyEnumerable
     else
       self.each { |el| return false unless el }
     end
-    return true
+    true
   end
 
   def each_cons(n)
-    if block_given?
-      enum = self.each
-      begin        
-        enum.each do 
-          array = []
-          n.times { array << enum.next }
-          yield array
-        end
-      rescue
-        return nil
-      end
+    return self.enum_for(:each) unless block_given?
+    enum, cnt = self.each, 0
+    while cnt <= (enum.size - n)
+      enum.rewind
+      cnt.times { enum.next }
+      array = []
+      n.times { array << enum.next }
+      yield array
+      cnt += 1
     end
-    nil
   end
 
   def include?(element)
-    self.each {|el| return true if el == element}
-    return false
+    self.each { |el| return true if el == element }
+    false
   end
 
   def count(element = nil)
@@ -66,7 +70,10 @@ module MyEnumerable
     if element
       self.each { |e| cnt += 1 if element == e }
     else
-      self.each { |e| cnt += 1 }
+      self.each do |e|
+        should_inc =  block_given? ? yield(e) : true
+        cnt += 1 if should_inc
+      end
     end
     cnt
   end
@@ -76,4 +83,113 @@ module MyEnumerable
     self.each { cnt += 1 }
     cnt
   end
+
+  # Groups the collection by result of the block.
+  # Returns a hash where the keys are the evaluated
+  # result from the block and the values are arrays
+  # of elements in the collection that correspond to
+  # the key.
+  def group_by
+    return self.enum_for(:each) unless block_given?
+    hash = Hash.new([])
+    self.each do |e|
+      res = yield e
+      hash[res] = Array.new unless hash.key?(res)
+      hash[res] << e
+    end
+    hash
+  end
+
+  def min
+    enum = self.each
+    cnt, min_val = enum.size, enum.next
+    while cnt > 1
+      cur = enum.next
+      i_min = block_given? ? yield(min_val, cur) : min_val <=> cur
+      min_val = cur if i_min == 1
+      cnt -= 1
+    end
+    min_val
+  end
+
+  def min_by
+    return self.enum_for(:each) unless block_given?
+    self.min { |a, b| yield(a) <=> yield(b) }
+  end
+
+  def max
+    enum = self.each
+    cnt, max_val = enum.size, enum.next
+    while cnt > 1
+      cur = enum.next
+      i_max = block_given? ? yield(max_val, cur) : max_val <=> cur
+      max_val = cur if i_max == -1
+      cnt -= 1
+    end
+    max_val
+  end
+
+  def max_by
+    return self.enum_for(:each) unless block_given?
+    self.max { |a, b| yield(a) <=> yield(b) }
+  end
+
+  def minmax
+    ar = []
+    if block_given?
+      ar << self.min { |a, b| yield a, b }
+      ar << self.max { |a, b| yield a, b }
+    else
+      ar << self.min
+      ar << self.max
+    end
+  end
+
+  def minmax_by
+    return self.enum_for(:each) unless block_given?
+    ar = []
+    ar << self.min_by { |e| yield e }
+    ar << self.max_by { |e| yield e }
+  end
+
+  def take(n)
+    ar, cnt = [], 0
+    self.each do | el |
+      ar << el
+      cnt += 1
+      return ar if cnt == n
+    end
+    ar
+  end
+
+  def take_while
+    return self.enum_for(:each) unless block_given?
+    ar = []
+    self.each do | e |
+      if yield(e)
+        ar << e 
+      else 
+        return ar
+      end
+    end
+    ar
+  end
+
+  def drop(n)
+    return [] if n >= self.size
+    ar, cnt = [], 0
+    self.each { |e| cnt += 1; ar << e if cnt > (self.size - n) }
+    ar
+  end
+
+  def drop_while
+    return self.enum_for(:each) unless block_given?
+    ar = []
+    self.each { | e | ar << e unless yield e }
+    ar
+  end
+
+  alias_method :collect, :map
+  alias_method :select, :filter
+  alias_method :foldl, :reduce
 end
--- week1/3-my-enumerable/solution_test.rb
+++ week1/3-my-enumerable/solution_test.rb
@@ -17,23 +17,37 @@ class SolutionTest < Minitest::Test
 
   def test_map
     collection = Collection.new(*1..5)
+    enum = collection.enum_for(:each)
 
     assert_equal [2, 3, 4, 5, 6], collection.map(&:succ)
-    assert_equal [6, 7, 8, 9, 10 ], collection.map {|e| e += 5}
+    assert_equal [6, 7, 8, 9, 10], collection.map { |e| e + 5 }
+    assert_equal enum.inspect, collection.map.inspect
+
+    assert_equal [2, 3, 4, 5, 6], collection.collect(&:succ)
+    assert_equal [6, 7, 8, 9, 10], collection.collect { |e| e + 5 }
+    assert_equal enum.inspect, collection.collect.inspect
   end
 
   def test_filter
     collection = Collection.new(*1..10)
+    enum = collection.enum_for(:each)
 
     assert_equal [1, 3, 5, 7, 9], collection.filter(&:odd?)
     assert_equal [2, 4, 6, 8, 10], collection.filter(&:even?)
+    assert_equal enum.inspect, collection.filter.inspect
+
+    assert_equal [1, 3, 5, 7, 9], collection.select(&:odd?)
+    assert_equal [2, 4, 6, 8, 10], collection.select(&:even?)
+    assert_equal enum.inspect, collection.select.inspect
   end
 
   def test_reject
     collection = Collection.new(*1..10)
+    enum = collection.enum_for(:each)
 
     assert_equal [1, 3, 5, 7, 9], collection.reject(&:even?)
     assert_equal [2, 4, 6, 8, 10], collection.reject(&:odd?)
+    assert_equal enum.inspect, collection.reject.inspect
   end
 
   def test_reduce
@@ -41,6 +55,9 @@ class SolutionTest < Minitest::Test
 
     assert_equal 55, collection.reduce(0) { |sum, n| sum + n }
     assert_equal 65, collection.reduce(0) { |sum, n| sum + n + 1 }
+
+    assert_equal 55, collection.foldl(0) { |sum, n| sum + n }
+    assert_equal 65, collection.foldl(0) { |sum, n| sum + n + 1 }
   end
 
   def test_include?
@@ -67,14 +84,14 @@ class SolutionTest < Minitest::Test
     collection2 = Collection.new(*0..6)
     collection3 = Collection.new(1, false, 0, 4, 5, 6)
 
-    assert_equal (true), (collection.all? {|e| e > 0})
-    assert_equal (false), (collection2.all? {|e| e > 0})
-    assert_equal (true), (collection2.all?)
-    assert_equal (false), (collection3.all?)
+    assert_equal(true, collection.all? {|e| e > 0})
+    assert_equal(false, collection2.all? {|e| e > 0})
+    assert_equal(true, collection2.all?)
+    assert_equal(false, collection3.all?)
   end
 
   def test_size
-    col = Collection.new()
+    col = Collection.new
     col_1 = Collection.new(1,2)
     col_2 = Collection.new([1, 2, 3], 2)
 
@@ -85,17 +102,127 @@ class SolutionTest < Minitest::Test
 
   def test_count
     collection = Collection.new(*1..10)
-    collection2 = Collection.new(1,1,1,2,3,4,4,4)
+    collection_2 = Collection.new(1, 1, 1, 2, 3, 4, 4, 4)
 
     assert_equal 1, collection.count(5)
     assert_equal 1, collection.count(9)
     assert_equal 10, collection.count(nil)
-    assert_equal 3, collection2.count(4)
+    assert_equal 3, collection_2.count(4)
+    assert_equal 5, collection_2.count { |e| e > 1}
+    assert_equal 3, collection_2.count { |e| e == 4}
   end
 
   def test_each_cons
     collection = Collection.new(*1..5)
-    collection.each_cons(2) { | e | p e }
+    array = []
+    enum = collection.enum_for(:each)
+
+    collection.each_cons(2) { |e| array << e }
+
+    assert_equal [1, 2], array[0]
+    assert_equal [2, 3], array[1]
+    assert_equal [3, 4], array[2]
+    assert_equal [4, 5], array[3]
+    assert_equal nil, array[4]
+
+    array.clear
+    collection.each_cons(3) { |e| array << e }
+
+    assert_equal [1, 2, 3], array[0]
+    assert_equal [2, 3, 4], array[1]
+    assert_equal [3, 4, 5], array[2]
+    assert_equal nil, array[3]
+
+    assert_equal enum.inspect, collection.each_cons(2).inspect
   end
+
+  def test_group_by
+    collection = Collection.new(*1..6)
+    enum = collection.enum_for(:each)
+
+    assert_equal({ 0=>[3, 6], 1=>[1, 4], 2=>[2, 5] }, collection.group_by { |i| i%3 })
+    assert_equal({ 0=>[2, 4, 6], 1=>[1, 3, 5]}, collection.group_by { |i| i%2 })
+    assert_equal enum.inspect, collection.group_by.inspect
+  end
+
+  def test_min
+    collection = Collection.new("albatross", "dog", "horse")
+
+    assert_equal "dog", collection.min { |a, b| a.length <=> b.length }
+    assert_equal "albatross", collection.min { |a, b| a <=> b }
+    assert_equal "albatross", collection.min
+  end
+
+  def test_min_by
+    collection = Collection.new("albatross", "dog", "horse")
+    enum = collection.enum_for(:each)
+
+    assert_equal("dog", collection.min_by { |a| a.length })
+    assert_equal enum.inspect, collection.min_by.inspect
+  end
+
+  def test_max
+    collection = Collection.new("albatross", "dog", "horse")
+
+    assert_equal "albatross", collection.max { |a, b| a.length <=> b.length }
+    assert_equal "horse", collection.max { |a, b| a <=> b }
+    assert_equal "horse", collection.max
+  end
+
+  def test_max_by
+    collection = Collection.new("albatross", "dog", "horse")
+    enum = collection.enum_for(:each)
+
+    assert_equal("albatross", collection.max_by { |a| a.length })
+    assert_equal("horse", collection.max_by { |a| a })
+    assert_equal enum.inspect, collection.max_by.inspect
+  end
+
+  def test_take
+    collection = Collection.new(*1..6)
+
+    assert_equal [1, 2, 3], collection.take(3)
+    assert_equal [1, 2, 3, 4, 5, 6], collection.take(8)
+  end
+
+  def test_take_while
+    collection = Collection.new(*1..6)
+    enum = collection.enum_for(:each)    
+
+    assert_equal [1, 2, 3], collection.take_while { |e| e < 4}
+    assert_equal enum.inspect, collection.take_while.inspect
+  end
+
+  def test_drop
+    collection = Collection.new(*1..6)
+
+    assert_equal [4, 5, 6], collection.drop(3)
+    assert_equal [], collection.drop(8)
+  end
+
+  def test_drop_while
+    collection = Collection.new(*1..6)
+    enum = collection.enum_for(:each)    
+
+    assert_equal [4, 5, 6], collection.drop_while { |e| e < 4}
+    assert_equal enum.inspect, collection.drop_while.inspect
+  end
+
+  def test_minmax
+    collection = Collection.new("albatross", "dog", "horse")
+
+    assert_equal ["dog", "albatross"], collection.minmax { |x, y| x.length <=> y.length }
+    assert_equal ["albatross", "horse"], collection.minmax
+  end
+
+  def test_minmax_by
+    collection = Collection.new("albatross", "dog", "horse")
+    enum = collection.enum_for(:each)
+
+    assert_equal ["dog", "albatross"], collection.minmax_by { |x| x.length }
+    assert_equal ["albatross", "horse"], collection.minmax_by { |x| x }
+    assert_equal enum.inspect, collection.minmax_by.inspect
+  end
+
 end
 
