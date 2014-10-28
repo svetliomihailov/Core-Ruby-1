3979f82e6ae1b5a571ad2ecddca8a4091ea6e165
Svetlozar Mihailov <svetliomihailov@gmail.com>
2014-10-28 21:36:20 +0200

    added yml file with tracks..

---
+++ week2/2-tracks/solution.rb
@@ -0,0 +1,26 @@
+class Track
+  attr_accessor :artist, :name, :album, :genre
+
+  def initialize(*args)
+    if args.length == 4
+      @artist = args[0]
+      @name = args[1]
+      @album = args[2]
+      @genre = args[3]
+    elsif args.length == 1
+      fail(ArgumentError, "You need to provide artist, name, album and genre of the track.
+They may be formated as a hash or as 4 separate arguments in that order") \
+        unless args[0].instance_of? Hash
+      @artist = args[0][:artist]
+      @name = args[0][:name]
+      @album = args[0][:albun]
+      @genre = args[0][:genre]
+    else
+      fail(ArgumentError, "You need to provide artist, name, album and genre of the track.
+They may be formated as a hash or as 4 separate arguments in that order")
+    end
+  end
+end
+
+class Playlist
+end
--- week2/2-tracks/solution_test.rb
+++ week2/2-tracks/solution_test.rb
@@ -1,9 +1,25 @@
 require 'minitest/autorun'
+require 'yaml'
 
 require_relative 'solution'
 
 class SolutionTest < Minitest::Test
-  def test_the_truth
-    assert true
+  def setup
+    @config = YAML.load_file('tracks.yml')  
+  end
+
+  def teardown
+    @config = nil
+  end
+
+  def test_track_create
+    track = Track.new "KAYTRANADA feat. Shay Lia", "Leave me alone", "So Bad", "Dance"
+    track_2 = Track.new artist: "KAYTRANADA feat. Shay Lia",
+                        name: "Leave me alone",
+                        album: "So Bad",
+                        genre: "Dance"
+
+    track_3 = Track.new @config["two"]
+    track_4 = Track.new "ba'", "wa"
   end
 end
