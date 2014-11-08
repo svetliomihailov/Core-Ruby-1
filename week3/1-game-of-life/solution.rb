8de031d03933a936c8c0c49a0c950d53f311479d
Svetlozar Mihailov <svetliomihailov@gmail.com>
2014-11-09 00:07:32 +0200

    Some work done on the game of life

--- week3/1-game-of-life/solution.rb
+++ week3/1-game-of-life/solution.rb
@@ -1,12 +1,103 @@
 module GameOfLife
+  CELL_LIVES = 1
+  CELL_DEAD = nil
+
   class Board
-    def initialize
+    def initialize(*args)
+      @b = {}
+      args.each do |a|
+        fail ArgumentError, 'Wrong argument type!' \
+          unless a.instance_of?(Array) && a.length == 2
+        @b.merge! Cell.new(a[0], a[1]) => CELL_LIVES
+      end
     end
 
-    def [](x ,y)
+    def [](x, y)
+      @b.key? Cell.new(x, y)
     end
 
     def each
+      return @b.enum_for(:each) unless block_given?
+      @b.each { |k, v|yield k, v }
+    end
+
+    def count
+      @b.size
+    end
+
+    def next_generation
+      # Returns new Board with the living cells
+    end
+
+    def cell_dies?(cell)
+      cnt = cell_neighbours cell
+      return false if cnt == 2 || cnt == 3
+      true
+    end
+
+    def cell_lives?(cell)
+      cnt = cell_neighbours cell
+      return true if cnt == 2 || cnt == 3
+      false
+    end 
+
+    private
+    
+    def this_cell_clumn(cell)
+      cnt = 0
+      cnt += 1 if @b.key? Cell.new(cell.x, cell.y + 1)
+      cnt += 1 if @b.key? Cell.new(cell.x, cell.y - 1)
+      cnt
+    end
+
+    def right_cell_column(cell)
+      cnt = 0
+      cnt += 1 if @b.key? Cell.new(cell.x + 1, cell.y + 1)
+      cnt += 1 if @b.key? Cell.new(cell.x + 1, cell.y)
+      cnt += 1 if @b.key? Cell.new(cell.x + 1, cell.y - 1)
+      cnt
+    end
+
+    def left_cell_column(cell)
+      cnt = 0
+      cnt += 1 if @b.key? Cell.new(cell.x - 1, cell.y + 1)
+      cnt += 1 if @b.key? Cell.new(cell.x - 1, cell.y)
+      cnt += 1 if @b.key? Cell.new(cell.x - 1, cell.y - 1)
+      cnt
+    end
+
+    def cell_neighbours(cell)
+      left_cell_column(cell) + right_cell_column(cell) + this_cell_clumn(cell)
+    end
+  end
+
+  class Cell
+    def initialize(x, y)
+      fail(ArgumentError, 'x and y must be integers!') \
+        unless x.is_a?(Integer) && y.is_a?(Integer)
+      @c = [x, y]
+    end
+
+    def x
+      @c[0]
+    end
+
+    def y
+      @c[1]
+    end
+
+    def ==(other)
+      fail ArgumentError, 'Cell type expected' unless other.instance_of? Cell
+      @c[0] == other.x && @c[1] == other.y
+    end
+
+    def eql?(other)
+      return false unless other.instance_of? Cell
+      hash == other.hash
+    end
+
+    def hash
+      @c.hash
     end
   end
 end
--- week3/1-game-of-life/solution_test.rb
+++ week3/1-game-of-life/solution_test.rb
@@ -2,8 +2,72 @@ require 'minitest/autorun'
 
 require_relative 'solution'
 
-class BoardTest < Minitest::Test
-  def test_the_truth
-    assert true
+class CellClassTest < Minitest::Test
+  include GameOfLife
+
+  def test_cell_creation
+    assert_raises(ArgumentError) { Cell.new 2, 'a' }
+    assert_raises(ArgumentError) { Cell.new 'b', 1 }
+  end
+
+  def test_cell_equal
+    cell_1, cell_2 = Cell.new(1, 2), Cell.new(1, 2)
+    cell_3 = Cell.new(2, 2)
+    assert_equal true, cell_1 == cell_2
+    assert_equal false, cell_1 == cell_3
+    assert_raises(ArgumentError) { Cell.new(1, 2) == 'Cell' }
+  end
+
+  def test_cell_eql?
+    cell_1, cell_2 = Cell.new(1, 2), Cell.new(1, 2)
+    cell_3 = Cell.new(2, 2)
+    assert_equal true, cell_1.eql?(cell_2)
+    assert_equal false, cell_1.eql?(cell_3)
+    assert_equal false, cell_1.eql?('Cell')
+  end
+
+  def test_cell_hash
+    cell_1, cell_2 = Cell.new(-45, 52), Cell.new(-45, 52)
+    cell_3, cell_4 = Cell.new(-45, -45), Cell.new(27, 52)
+    assert_equal true, cell_1.hash == cell_2.hash
+    assert_equal false, cell_1.hash == cell_3.hash
+    assert_equal false, cell_1.hash == cell_4.hash
+  end
+end
+
+class BoardClassTest < Minitest::Test
+  include GameOfLife
+
+  def new_board
+    Board.new [0, 0], [0, 1], [1, 0], [1, 2], [2, 1], [3, 2]
+  end
+
+  def test_board_creation
+    assert_raises(ArgumentError) { Board.new [1, 2], [2, 1], [3, 2, 3] }
+  end
+
+  def test_board_index
+    b = new_board
+    assert_equal true, b[1, 2]
+    assert_equal true, b[2, 1]
+    assert_equal false, b[3, 4]
+  end
+
+  def test_board_count
+    b = new_board
+    assert_equal 6, b.count
+  end
+
+  def test_board_each
+    b = new_board
+    enum = b.each
+    assert_equal true, enum.instance_of?(Enumerator)
+    enum.each { |_k, v| assert_equal false, v.nil? }
+    b.each { |_k, v| assert_equal false, v.nil? }
+  end
+
+  def test_board_next_generation
+    b = new_board
+    b.cell_lives? Cell.new(0, 1)
   end
 end
